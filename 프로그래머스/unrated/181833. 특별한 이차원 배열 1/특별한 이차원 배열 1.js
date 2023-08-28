0eec2869e842f1d781b63e5f87f5b76aebb9a768
byunjiin <129635857+byunjiin@users.noreply.github.com>
2023-08-29 03:00:26 +0900

    Update 특별한 이차원 배열 1.js

--- 프로그래머스/unrated/181833. 특별한 이차원 배열 1/특별한 이차원 배열 1.js
+++ 프로그래머스/unrated/181833. 특별한 이차원 배열 1/특별한 이차원 배열 1.js
@@ -1,9 +1,12 @@
+//2차원배열을 1차씩 쪼개서 품(answer안에 arr배열)
+//n=3이면 answer[arr[] arr[] arr[]]
 function solution(n) {
     var answer = [];
-        for(let a=0; a<n; a++){ 
-            var arr = new Array(n).fill(0); //0채우기
-            arr[a] = 1; //1넣기 (i=j일때)
-            answer.push(arr);
+        for(let x=0; x<n; x++){ 
+            var arr = new Array(n).fill(0); //arr 배열요소를 전부 0채우기
+            //2차원을 1차원으로 생각하니 i=j일땐 arr[0],arr[1],arr[2]가 됨
+            arr[x] = 1; //1넣기
+            answer.push(arr); //완성한 arr배열을 answer안에 넣기
         }
     return answer;
 }
