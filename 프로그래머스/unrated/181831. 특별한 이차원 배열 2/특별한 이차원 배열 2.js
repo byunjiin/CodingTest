492bad80666884a1a3e4f7bc2c969b73d8e3b395
byunjiin <129635857+byunjiin@users.noreply.github.com>
2023-08-29 14:49:34 +0900

    Update 특별한 이차원 배열 2.js

--- 프로그래머스/unrated/181831. 특별한 이차원 배열 2/특별한 이차원 배열 2.js
+++ 프로그래머스/unrated/181831. 특별한 이차원 배열 2/특별한 이차원 배열 2.js
@@ -1,3 +1,6 @@
+//배열의 기본 리턴값을 1로 지정하고, 조건이 거짓일경우 0으로 반환
+//예시 참 : a[0][1]=1 a[1][0]=1 ->return 1
+//예시 거짓 : a[0][1]=1 a[1][0]=3 ->return 0
 function solution(arr) {
     var answer = 1;
     for(let i=0; i<arr.length; i++){
