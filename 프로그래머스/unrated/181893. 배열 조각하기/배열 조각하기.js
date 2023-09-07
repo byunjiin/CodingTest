d5b8bd9804fe9a5bb009eb7a6948635b0058b57b
byunjiin <129635857+byunjiin@users.noreply.github.com>
2023-09-08 00:44:28 +0900

    [unrated] Title: 배열 조각하기, Time: 0.20 ms, Memory: 33.7 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/181893. 배열 조각하기/배열 조각하기.js
@@ -0,0 +1,10 @@
+function solution(arr, query) {
+    for(let i=0;i<query.length;i++){
+        if(i%2 ===0){//짝수 인덱스
+            arr=arr.slice(0,query[i]+1); //0~i
+        }else{//홀수 인덱스
+            arr=arr.slice(query[i], ); //i~끝
+        }
+    }
+    return arr;
+}
